033ab73c8a3c6b7237157a98cacba4607e57cbf0
Amen <aymantauhid2@gmail.com>
2022-03-11 18:29:42 +0530

    Added the req packages.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
+import plotly.express as px
 
 DATA_URL = (
 "/Users/AT/OneDrive/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv")
